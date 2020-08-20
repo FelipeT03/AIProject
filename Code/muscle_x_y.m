27c9d579c12527cef2f38ed5f6cf0280e3c91363
FelipeT03 <ftoscano03@gmail.com>
2020-08-20 18:31:49 -0500

    msucle_x_y improved

--- Code/muscle_x_y.m
+++ Code/muscle_x_y.m
@@ -2,18 +2,15 @@ function [muscle_x, muscle_y, muscle_x_min, muscle_x_max] = muscle_x_y(eco)
 %Encuentra en los centroides para el cálculo de la Apo inferior
     %% Parameters
     K = 2;
-    %img_size_eco = size(eco);
     %% Tratamiento de datos
-    %I = im2single(eco);
+    I = im2single(eco);
     
     %% Training
-    %[L,centers] = imsegkmeans(I,K);
-    %centers = sort(centers);
-    %idx = findClosestCentroids(eco(:), centers);
-    %L = reshape(idx, img_size_eco(1), img_size_eco(2), 1);
-    eco_C = eco > 0.1;
+    [L,centers] = imsegkmeans(I,K);
+    
     %% Results
-    %eco_C = L == K;
+    [val, p_val] = max(centers); 
+    eco_C = L == p_val;
     eco_C = findLargestArea(eco_C);
     [row,col] = find(eco_C);
     muscle_y = round((min(row) + max(row)) / 2);
