fa0926cae0c409cf3700ad321d2e25493f4fba4f
FelipeT03 <ftoscano03@gmail.com>
2020-10-15 08:56:55 -0500

    txt en save

--- Code/Functions/SaveResults.m
+++ Code/Functions/SaveResults.m
@@ -17,5 +17,12 @@ function SaveResults(memoria_fascia_sup_inf, eco_memory, Results, MT_value, vide
     waitbar(0.9,App_Status,'Saving Results');  
     writetable(Results_table,strcat(path_results,'/Summary.csv'))
     waitbar(1,App_Status,'Saving Results');  
+    fileID = fopen(strcat(path_results,'/README.txt'),'w');
+    fprintf(fileID,'eco_memory.mat: It contains all the frames of the video. It is used by the GUI. \r\n');
+    fprintf(fileID,'memoria_fascia_sup_inf.mat: It contains the locations of both fascias in millimeters. \r\n');
+    fprintf(fileID,'Results.mat: It contains a summary of the analysis results. If you change parameters in the GUI these values will change. \r\n');
+    fprintf(fileID,'Summary.csv: It contains all the information on Results.mat in .csv format. \r\n');
+    fprintf(fileID,'Thickness.csv: It contains all the information on the Muscle Thickness plot. \r\n');
+    fclose(fileID);
     close(App_Status)
 end
