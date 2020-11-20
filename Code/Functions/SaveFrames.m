b70e57a0cb2192fce345205be74ca8de88d6a4a7
FelipeT03 <ftoscano03@gmail.com>
2020-11-19 21:52:50 -0500

    Modificacion fascia sup

--- Code/Functions/SaveFrames.m
+++ Code/Functions/SaveFrames.m
@@ -10,8 +10,8 @@ function SaveFrames(eco_memory,measure_point,video_name,path_frames, scale_facto
                      0 0 0 1 0 0 0];
     eco_memory(2:5,measure_point - 3:measure_point + 3,:) = triangle_down + zeros(1,1,size(eco_memory,3));
     for k = 1:size(eco_memory,3)
-        RGB = insertText(eco_memory(:,:,k),[0 0],compose(num2str(k)+"\n"+str)); %print frame# and scale on picture
-        imwrite(RGB,strcat(path_images,'\',num2str(k),'.jpg'));
+        %RGB = insertText(eco_memory(:,:,k),[0 0],compose(num2str(k)+"\n"+str)); %print frame# and scale on picture
+        imwrite(eco_memory(:,:,k),strcat(path_images,'\',num2str(k),'.jpg'));
         waitbar(k*Waitbar_value,App_Status,'Saving Frames...');
     end 
     waitbar(1,App_Status,'Complete');
