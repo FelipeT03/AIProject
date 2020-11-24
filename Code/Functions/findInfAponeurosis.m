3ab3bae94a41d6f943c7924c07924d44386e7c05
FelipeT03 <ftoscano03@gmail.com>
2020-11-23 21:12:27 -0500

    Version funcional para la mayoria de errores

--- Code/Functions/findInfAponeurosis.m
+++ Code/Functions/findInfAponeurosis.m
@@ -2,32 +2,25 @@ function [x_InfApo,y_InfApo] = findInfAponeurosis(eco,t_centroids)
 %Encuentra el límite inferior de la fascia inferior y devuelve su posición
 %en un vector(x,y) utilizando los centroides de luminancia proporcionados
     eco_C = eco > t_centroids;
-    img_size_eco = size(eco);
     %% Tratamiento de la imagen hasta conseguir una sola figura 
     eco_C = bwareaopen(eco_C,400);%400
     CC = bwconncomp(eco_C, 8);
     S = regionprops(CC, 'Area');
     Areas = sort([S.Area],'descend');
-    if length([S.Area]) >= 2
-        eco_C = bwareaopen(eco_C,Areas(2));
-    else 
-        eco_C = bwareaopen(eco_C,Areas(length([S.Area])));
-    end
-       
-    eco_C = eco_C(1:img_size_eco(1),1:img_size_eco(2));
-    
+    eco_C = bwareaopen(eco_C,round(Areas(1) * 0.3));
 
     %% Encontrar los puntos que delimitan la figura
     %0.8 del promedio, debe superar este valor para ser considerado como
     %parte de la fascia, si es inferior a este valor se asigna el valor
     %promedio.
+    eco_C(1,:) = 1;
     [row,col] = find(eco_C);
     [C,IA] = unique(col,'last');
     vector = [C row(IA)];
-    ajuste = mean(vector(round(end*0.6):round(end*0.9),2));
+    ajuste = mean(vector(round(end*0.2):round(end*0.8),2));
     x_InfApo = 1:size(eco,2);
     y_InfApo = zeros(size(eco,2),1) + ajuste;
-    y_InfApo(vector(:,2) > (0.8*ajuste)) = vector(vector(:,2) > (0.8*ajuste),2);
+    y_InfApo(vector(:,2) > (0.5*ajuste)) = vector(vector(:,2) > (0.5*ajuste),2);
     y_InfApo = medfilt1(y_InfApo,3);
 end
 
--- Code/Functions/findSupAponeurosis.m
+++ Code/Functions/findSupAponeurosis.m
@@ -3,12 +3,14 @@ function [x_SupApo,y_SupApo] = findSupAponeurosis(eco, t_centroids)
 %en un vector(x,y) utilizando los centroides de luminancia proporcionados
     eco_C = eco > t_centroids;
     %% Tratamiento de la imagen hasta conseguir una sola figura 
-    eco_C = bwareaopen(eco_C,500);
+    CC = bwconncomp(eco_C, 8);
+    S = regionprops(CC, 'Area');
+    Areas = sort([S.Area],'descend');
+    eco_C = bwareaopen(eco_C,round(Areas(1) * 0.50));
     %% Encontrar los puntos que delimitan la figura
     [row,col] = find(eco_C);  
     [C,IA] = unique(col,'first');
     vector = [C row(IA)];
-    
     x_SupApo = 1:size(eco,2);
     y_SupApo = zeros(size(eco,2),1) + mean(vector(:,2));
     y_SupApo(vector(:,1)) = vector(:,2);
